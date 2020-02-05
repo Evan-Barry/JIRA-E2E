04315f3ba3613750d3f3bf537dde97742728eb08
Evan Barry <evbarry@fexco.ie@ftsliu0125c.fexco.ie>
2020-02-05 11:23:50 +0000

    updated JIRA-E2E.py

--- JIRA-E2E.py
+++ JIRA-E2E.py
@@ -8,8 +8,8 @@ import sys
 issue_name = "helloWorldScreen"
 #issue_id = sys.argv[1] #works with argument passed in
 #issue_name = sys.argv[1] #works with argument passed in
-url_name = "http://localhost:8090/rest/api/2/search?jql=summary~"  # must get JIRA address from jenkins?
-url_id = "http://localhost:8090/rest/api/2/issue/"
+url_name = "http://localhost:8091/rest/api/2/search?jql=summary~"  # must get JIRA address from jenkins?
+url_id = "http://localhost:8091/rest/api/2/issue/"
 save_path = ""
 headers = {
         'Authorization': 'Basic dDAwMjAyMzc2OjVlOWQ5eWt5'  # must have proper authentication? OAUTH
